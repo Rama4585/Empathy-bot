0933f56d4ccc43daa06b9ff0f6ac84ed66ec5d7b
Rama4585 <balarama.5553535@gmail.com>
2026-06-09 18:14:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,20 +70,20 @@ PACKAGES = [
 #=========================
 
 def get_main_menu(balance):
-return InlineKeyboardMarkup(
-inline_keyboard=[
-[InlineKeyboardButton(text=f"🎤 Разобрать голосовое • осталось {balance}", callback_data="send_audio")],
-[InlineKeyboardButton(text="✨ Посмотреть пример", callback_data="example")],
-[InlineKeyboardButton(text="💎 Получить ещё попытки", callback_data="buy_menu")],
-[InlineKeyboardButton(text="🚀 Подписаться на канал", callback_data="channel")],
-[InlineKeyboardButton(text="❓ Как это работает", callback_data="how_it_works")]
-]
-)
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text=f"🎤 Разобрать голосовое • осталось {balance}", callback_data="send_audio")],
+            [InlineKeyboardButton(text="✨ Посмотреть пример", callback_data="example")],
+            [InlineKeyboardButton(text="💎 Получить ещё попытки", callback_data="buy_menu")],
+            [InlineKeyboardButton(text="🚀 Подписаться на канал", callback_data="channel")],
+            [InlineKeyboardButton(text="❓ Как это работает", callback_data="how_it_works")]
+        ]
+    )
 
 def get_back_menu():
-return InlineKeyboardMarkup(
-inline_keyboard=[[InlineKeyboardButton(text="🔙 Назад", callback_data="main_menu")]]
-)
+    return InlineKeyboardMarkup(
+        inline_keyboard=[[InlineKeyboardButton(text="🔙 Назад", callback_data="main_menu")]]
+    )
 
 #=========================
 
