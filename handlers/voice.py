84971331407bff7aac5bcae68ce33bdc3a8519fd
Rama4585 <balarama.5553535@gmail.com>
2026-06-15 03:37:00 +0300

    Update voice.py

--- handlers/voice.py
+++ handlers/voice.py
@@ -24,7 +24,7 @@ async def invoice(callback: CallbackQuery):
     _, price, amount = callback.data.split("_")
     await bot.send_invoice(
         chat_id=callback.message.chat.id,
-        title=f"{amount} разборов",
+        title=f"{amount} мин обработки",
         description="Разбор голосовых",
         payload=f"add_{amount}",
         currency="XTR",
@@ -38,32 +38,40 @@ async def pre_checkout(query: PreCheckoutQuery):
 @router.message(F.successful_payment)
 async def payment(message: Message):
     amount = int(message.successful_payment.invoice_payload.split("_")[1])
-    await update_balance(message.from_user.id, amount)
-    await message.answer(f"✅ Добавлено {amount} разборов")
+    await update_balance(message.from_user.id, amount * 60)
+    await message.answer(f"✅ Добавлено {amount} мин обработки")
 
 @router.message(F.voice)
 async def voice(message: Message):
     uid = message.from_user.id
     if await get_balance(uid) <= 0:
-        await message.answer("⚠️ Разборы закончились. Пополните баланс через меню старт /start")
+        await message.answer("⏱ Время разбора закончилось\n\nМожно получить ещё разборы или пригласить друга 🎁")
         return
-    status = await message.answer("🎧 Слушаю... ▰▱▱▱▱")
+    await message.answer("🎧 Голосовое получено\n⏳ Начинаю разбор...")
     file = await bot.get_file(message.voice.file_id)
     path = f"voice_{message.voice.file_id}.ogg"
     await bot.download_file(file.file_path, path)
-    try:
-        transcript, answer = await asyncio.to_thread(process_audio, path)
-        await update_balance(uid, -1)
-        new_bal = await get_balance(uid)
-        kb = InlineKeyboardMarkup(inline_keyboard=[
+try:
+    transcript, answer = await asyncio.to_thread(process_audio, path)
+    await update_balance(uid, -message.voice.duration)
+
+    new_bal = await get_balance(uid)
+
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[
             [InlineKeyboardButton(text="🔁 Разобрать ещё одно", callback_data="main_menu")],
-            [InlineKeyboardButton(text="💎 Купить попытки", callback_data="buy_menu")]
-        ])
-        await message.answer(f"📝 **Результат разбора:**\n\n{answer}\n\n✨ Осталось: {new_bal}", reply_markup=kb)
-    except Exception as e:
+            [InlineKeyboardButton(text="✨ Получить ещё разборы", callback_data="packages")]
+        ]
+    )
+
+    await message.reply(
+        f"📝 **Результат разбора:**\n\n{answer}\n\n⏱ Осталось: {round(new_bal / 60, 1)} мин",
+        reply_markup=kb
+    )
+
+except Exception as e:
         print(f"Error: {e}") 
         await message.answer("⚠️ Ошибка при анализе. Попробуйте позже.")
-    finally:
+finally:
         if os.path.exists(path): os.remove(path)
-        await status.delete()
 
